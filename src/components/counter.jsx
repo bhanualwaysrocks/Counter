c9c6ee6ee4541703d0a18fa71cf071784e10fc6f
BhanuSankhla <sankhlabhanupratap@gmail.com>
2022-10-22 22:06:12 +0530

    Fixed event handler to take argument in it.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -14,7 +14,7 @@ class Counter extends Component {
         return (
             <div>
                 <span className={this.getClasses()}>{this.formatCount()}</span>
-                <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
+                <button onClick={() =>this.handleIncrement()} className='btn btn-secondary btn-sm'>Increment</button>
             </div>
         );
     }
