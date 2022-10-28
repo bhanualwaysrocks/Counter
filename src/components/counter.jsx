88de6d3abf74c8ba220e547259cb68d8d1726f1b
BhanuSankhla <sankhlabhanupratap@gmail.com>
2022-10-28 12:28:48 +0530

    Added Counters component and added reset, increment and decrement functionality in it.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,34 +1,45 @@
-import { getByPlaceholderText } from '@testing-library/react';
-import React, { Component } from 'react';
+import { getByPlaceholderText } from "@testing-library/react";
+import React, { Component } from "react";
 
 class Counter extends Component {
-    state = { 
-        count: 0,
-     }
+  // state = {
+  //     count: 0,
+  //  }
 
-    handleIncrement = () => {
-        this.setState({ count : this.state.count + 1});
-    }
+  // handleIncrement = () => {
+  //     this.setState({ count : this.state.count + 1});
+  // }
 
-    render() {
-        return (
-            <div>
-                <span className={this.getClasses()}>{this.formatCount()}</span>
-                <button onClick={() =>this.handleIncrement()} className='btn btn-secondary btn-sm'>Increment</button>
-            </div>
-        );
-    }
-    
-    formatCount() {
-        const { count } = this.state;
-        return (count === 0) ? "Zero" : count;
-    }
+  render() {
+    return (
+      <div>
+        <span className={this.getClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
 
-    getClasses() {
-        let Classes = "badge m-2 badge-";
-        Classes += (this.state.count === 0) ? "warning" : "primary";
-        return Classes;
-    }
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
+  }
+
+  getClasses() {
+    let Classes = "badge m-2 badge-";
+    Classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return Classes;
+  }
 }
- 
+
 export default Counter;
